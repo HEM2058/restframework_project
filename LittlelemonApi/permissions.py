99a985516a8343e5bbc09a6de56df168d72521bb
Hem Raj Pandey <hemrajpandeyd@gmail.com>
2023-10-15 14:49:33 +0545

    changed

--- LittlelemonApi/permissions.py
+++ LittlelemonApi/permissions.py
@@ -2,21 +2,20 @@ from rest_framework import permissions
 
 class ReadOnlyOrUnauthorized(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Check if the user is a member of the "Delivery crew" group
         user = request.user
-        if user and user.groups.filter(name='Delivery crew').exists():
-            # Allow GET requests (read-only)
-            if request.method in permissions.SAFE_METHODS:
+
+        if user:
+            if user.groups.filter(name='Delivery crew').exists():
+                # Allow GET requests (read-only)
+                if request.method in permissions.SAFE_METHODS:
+                    return True
+                # Deny POST, PUT, PATCH, DELETE requests
+                return False
+            elif user.groups.filter(name='Manager').exists():
+                # Allow GET, POST, PUT, PATCH, DELETE requests for Managers
                 return True
-            # Deny POST, PUT, PATCH, DELETE requests
-            return False
-        if user and user.groups.filter(name='Manager').exists():
-            # Allow GET requests (read-only)
-            if request.method in permissions.SAFE_METHODS:
-                return True
-            # allow POST, PUT, PATCH, DELETE requests
-            return True
-        # If the user is not a member of the "Delivery crew" group, consider them as customers
+
+        # If the user is not in the "Delivery crew" or "Manager" group, consider them as customers
         # Allow GET requests (read-only) for customers
         if request.method in permissions.SAFE_METHODS:
             return True
